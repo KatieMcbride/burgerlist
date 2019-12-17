3ce5d83240fe9dcd9db81c5cc9b5472b07d74636
Katie McBride <mcbride.katieb@gmail.com>
2019-12-16 22:05:48 -0600

    working put

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -8,21 +8,42 @@ var burger = require("../models/burgerFunctions.js");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.all(function(data) {
-    var hbsObject = {
+    var allBurgers = {
       burgers: data
     };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+    console.log(allBurgers);
+    res.render("index", allBurgers);
   });
 });
 
-// router.post("/api/", function(req, res) {
- 
-// });
+router.post("/api/", function(req, res) {
+    burger.create([
+        "name", "eaten"
+    ],
+    [
+        req.body.name, req.body.eaten
+    ],function(result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+    });
+});
 
-// router.put("/api/", function(req, res) {
-
-// });
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.update({
+      eaten: req.body.eaten
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
 
 // router.delete('/api/', function(req, res) {
  
